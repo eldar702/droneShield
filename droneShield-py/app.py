2032204e0c83401aedd2595f114e9b45630400f9
Eldar Shlomi <72104254+eldar702@users.noreply.github.com>
2021-10-26 17:48:50 +0300

    'change app.py and  create droneapp.service so app.py will run as linux service'

--- droneShield-py/app.py
+++ droneShield-py/app.py
@@ -1,10 +1,30 @@
-import logging, time
+###############################        Defines & Imports       #######################################
+import logging, time, argparse
 #checking
-logging.basicConfig(filename='./logs/' + str(time.asctime()) + '.log',
-                    filemode='w',
-                    level=logging.DEBUG,
-                    format='%(asctime)s %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
+# parser use to get the location of the app
+parser = argparse.ArgumentParser()
+parser.add_argument('--d', nargs=1, default=None)
+args = parser.parse_args()
+
+# the app directory location
+APP_DIR = args.d[0] if args.d != None else "./"
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler(APP_DIR + 'logs/main app | ' + str(time.asctime()) + '.log'),
+        logging.StreamHandler() ])
+
+
+#######################################      Main      ################################################  
 if __name__ == '__main__':
-    logging.debug('droneapp has started!')
+    logging.debug('droneapp has started! THE directory is %s', APP_DIR)
                     
+
+# logging.basicConfig(filename='./logs/' + str(time.asctime()) + '.log',
+#                     filemode='w',
+#                     level=logging.DEBUG,
+#                     format='%(asctime)s %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
