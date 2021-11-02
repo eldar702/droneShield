b9f2ea50e37d817a26379aeb5ab6ba5e1619e71d
Eldar Shlomi <72104254+eldar702@users.noreply.github.com>
2021-11-02 19:13:31 +0200

    add webSocketConfiguration, videoSocketHandler, videoStreamManager

---
+++ GroundStation/src/main/java/com/droneshield/cloudapp/service/VideoStreamManager.java
@@ -0,0 +1,5 @@
+package com.droneshield.cloudapp.service;
+/** The stream manager: saves the drones id with currespondeing session and receive incoming udp data packets  */
+public class VideoStreamManager {
+    
+}
